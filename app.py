6c26fb9f1710c8a9944afb79445508164ac576d4
Sebighiara <sebi.gg98@gmail.com>
2023-08-31 12:34:04 +0300

    add data to supabase table

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 from supabase import create_client
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 #load env vars
 load_dotenv()
@@ -15,9 +15,20 @@ app = Flask(__name__)
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
+    if request.method == "POST":
+        #get data from user
+        req = request.form
+        name = req.get("name")
+        last_name = req.get("last_name")
+
+        #insert the data from the user to supabase table
+        data = supabase.table("names").insert({"name": name, "last_name": last_name}).execute()
+
+        return redirect(request.url)
+
     return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
