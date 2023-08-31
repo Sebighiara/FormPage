5c973c0f0baf286f8c4182f519bbfebb2525c5d9
Sebighiara <sebi.gg98@gmail.com>
2023-08-31 12:08:27 +0300

    setup supabase

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
+from dotenv import load_dotenv
+import os
+from supabase import create_client
 from flask import Flask, render_template
+
+#load env vars
+load_dotenv()
+url = os.environ.get("SUPABASE_URL")
+key = os.environ.get("SUPABASE_KEY")
+
+#setup supabase client
+supabase = create_client(url, key)
 
 app = Flask(__name__)
 
